498ba50cb871f40826f8931849cdd03295999ac4
Usman Ali <usman_pervaiz@live.com>
2020-06-23 22:47:57 +0300

    First component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-     
+      <TodoList />
     </div>
   );
 }
