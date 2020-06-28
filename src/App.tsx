677449b3de03f05816e76a416751d1f752e21514
Usman Ali <usman_pervaiz@live.com>
2020-06-28 13:20:39 +0300

    state management & props concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList'
 import NewTodo from './components/NewTodo'
+import { Todo } from './todo.model'
+
 
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'Finish the course'}];
+  const [todos, setTodos] = useState<{ id: string, text: string }[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [...prevTodos, { id: Math.random().toString(), text: text }])
+  }
   return (
     <div className="App">
-      <NewTodo />
-      <TodoList items={todos}/>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
